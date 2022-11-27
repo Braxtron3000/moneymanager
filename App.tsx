b9de3103645391d1bbccafca127ff0303d06b424
braxton hancock <bdhancock@olivet.edu>
2022-11-26 19:25:49 -0500

    basic rough draft of the only screen

--- App.tsx
+++ App.tsx
@@ -8,8 +8,10 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
+import React, {useState, type PropsWithChildren} from 'react';
 import {
+  FlatList,
+  Image,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -19,43 +21,7 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -64,57 +30,48 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const firstOne = {amount: 0, topic: 'add new envelope'};
+  const [thearray, setArray] = useState<{amount: number; topic: string}[]>([
+    firstOne,
+  ]);
+
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={[backgroundStyle, styles.body]}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+      <View style={styles.header}>
+        <Text style={styles.headerText}>$1120</Text>
+        <Image source={require('./src/assets/piggy-bank.png')} />
+        <View style={{alignItems: 'center'}}>
+          <Text style={styles.headerText}>$230</Text>
+          <Text style={{fontSize: 10}}> unallocated for</Text>
         </View>
-      </ScrollView>
+      </View>
+      <FlatList
+        data={thearray}
+        renderItem={({item}) => <Text>{item.amount}</Text>}
+      />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  body: {
+    alignContent: 'center',
+    width: '100%',
+    height: '100%',
+    flexDirection: 'column',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  header: {
+    flexDirection: 'row',
+    width: '100%',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  headerText: {color: 'black'},
 });
 
 export default App;
---
+++ readme.md
@@ -0,0 +1,16 @@
+# what need does this project fill?
+
+I want to know how i manage my money and I dont want to spend a lot of time doing so.
+I think the best way to fix this is through the envelope method.
+
+For those that dont know the envelope method is a money managing technique where you filter your money into literal envelopes. So you could have an envelope for groceries, eating out, and another for housing etc.
+
+# outside of the above question's answer, what are your motives for creating this project?
+
+1. I want to display that I know the basics for building a simple react-native mobile application such as ...
+
+- views and components
+- redux
+- prisma
+
+2. and have a project that is actually complete for once & i can be proud of
